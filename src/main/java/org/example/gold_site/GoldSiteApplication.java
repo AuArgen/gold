2513e0bb04db892483b406ab741473ff29ccd1b2
AuArgen <argenazizbekuulu3@gmail.com>
2025-08-08 16:32:45 +0600

    create

---
+++ src/main/java/org/example/gold_site/GoldSiteApplication.java
@@ -0,0 +1,13 @@
+package org.example.gold_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldSiteApplication.class, args);
+    }
+
+}
